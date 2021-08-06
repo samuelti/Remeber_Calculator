c651470a197cfe1e00fc098716341e7d5028728e
samuelti <samuelti2442@gmail.com>
2021-08-05 21:36:02 -0700

    added server.js, models, and configs

---
+++ models/Calculations.js
@@ -0,0 +1,29 @@
+const {Model, DataTypes} = require('sequelize');
+
+const sequelize = require('../config/config.js');
+
+class Calculations extends Model {}
+
+Calculations.init(
+  {
+    id:{
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    calc_content: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'calculations',
+  }
+);
+
+module.exports = Calculations;
---
+++ models/UsersCalculations.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/config.js');
+
+class UsersCalculations extends Model {}
+
+UsersCalculations.init(
+  {
+    id:{
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    user_id:{
+      type: DataTypes.INTEGER, 
+      refrences:{
+        model: 'users',
+        key: 'id'
+      }
+    },
+    calculation_id:{
+      type: DataTypes.INTEGER, 
+      refrences:{
+        model: 'calculations',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'users_calculations',
+  }
+);
+
+module.exports = UsersCalculations;
