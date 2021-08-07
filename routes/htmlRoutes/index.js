5a88e8847e10fe8ec30cd7a10e64ff5a1b2e0cb5
samuelti <samuelti2442@gmail.com>
2021-08-07 15:25:25 -0700

    fixed minor routing error

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -4,8 +4,12 @@ const homepageRoute= require('./homepage.js');
 const signUpRoute = require('./signup.js');
 const loginRoute = require('./login.js');
 
+// router.use('/', homepageRoute);
+// router.use('/signUp', signUpRoute);
+// router.use('/login', loginRoute);
+
 router.use('/', homepageRoute);
-router.use('/signUp', signUpRoute);
-router.use('/login', loginRoute);
+router.use('/', signUpRoute);
+router.use('/', loginRoute);
 
 module.exports = router;
