9f4685fbecf361ca363dd890bcd2bb1785030746
samuelti <samuelti2442@gmail.com>
2021-08-07 14:37:54 -0700

    added route to get to the sign up page and homepage.

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
-const homepageRoute= require('./homepage.js')
+const homepageRoute= require('./homepage.js');
+const signUpRoute = require('./signup.js');
 
 router.use('/', homepageRoute);
+router.use('/signUp', signUpRoute);
 
 module.exports = router;
---
+++ routes/htmlRoutes/signup.js
@@ -0,0 +1,24 @@
+const Users = require('../../models/Users');
+const router = require('./homepage');
+
+router.post("/signUp", (req, res) => {
+
+    console.log("sign up data");
+  
+    console.log(req.body);
+  
+      Users.create(req.body).then(createData => {
+  
+          console.log(createData);
+  
+      res.redirect('/')
+  
+    });
+  
+  });
+
+router.get("/signUp", (req,res) => {
+    res.render('signup')
+});
+
+module.exports = router;
