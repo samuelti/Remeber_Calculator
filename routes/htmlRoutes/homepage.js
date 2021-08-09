ec9b3a2c28329bfb4807c9ba140fa1fe826717ce
samuelti <samuelti2442@gmail.com>
2021-08-08 21:43:15 -0700

    added homepage text explaining project

--- routes/htmlRoutes/homepage.js
+++ routes/htmlRoutes/homepage.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 // get all posts 
 router.get('/', (req, res) => {
     console.log('session',req.session);
-    res.render('homepage');
+    res.render('homepage', {username:req.session.username});
 });
 
 module.exports = router;
